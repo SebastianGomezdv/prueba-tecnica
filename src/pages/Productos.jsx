34a823bec3a0efb47b3935295c9878125e3207d1
jaimezzapata <zapataval2304@gmail.com>
2025-09-09 12:23:35 -0500

    Implementación de método DELETE

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { endpoints } from "../utils/api";
+import { alertaGeneral } from "../utils/alertas";
 const Productos = () => {
   const [getProductos, setProductos] = useState([]);
   function consultarProductos() {
@@ -15,6 +16,20 @@ const Productos = () => {
     consultarProductos();
   }, []);
 
+  function eliminarProducto(id) {
+    fetch(`${endpoints.productos}/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        alertaGeneral(
+          "Producto Eliminado",
+          "El producto se ha eliminado de forma correcta",
+          "success"
+        );
+        consultarProductos();
+      })
+      .catch((error) => console.log(error));
+  }
   return (
     <div className="cards-productos">
       {getProductos.map((producto) => (
@@ -23,6 +38,12 @@ const Productos = () => {
           <p>Descripción: {producto.descripción}</p>
           <p>Precio: ${producto.precio}</p>
           <p>Cantidad: {producto.cantidad}</p>
+          <div className="card-buttons">
+            <button type="button">Editar</button>
+            <button onClick={() => eliminarProducto(producto.id)} type="button">
+              Eliminar
+            </button>
+          </div>
         </article>
       ))}
     </div>
