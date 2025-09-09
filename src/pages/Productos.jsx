1364368245bcf7938e21fd1d4bc715df27e2edec
jaimezzapata <zapataval2304@gmail.com>
2025-09-09 10:57:47 -0500

    Recorrido de productos

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import { endpoints } from "../utils/api";
 const Productos = () => {
-  return (
-    <div>Productos</div>
-  )
-}
+  const [getProductos, setProductos] = useState([]);
+  function consultarProductos() {
+    fetch(endpoints.productos)
+      .then((response) => response.json())
+      .then((data) => {
+        setProductos(data);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  }
+  useEffect(() => {
+    consultarProductos();
+  }, []);
 
-export default Productos
+  return (
+    <div className="cards-productos">
+      {getProductos.map((producto) => (
+        <article key={producto.id} className="card-producto">
+          <h2>Nombre: {producto.nombre}</h2>
+          <p>Descripción: {producto.descripción}</p>
+          <p>Precio: ${producto.precio}</p>
+          <p>Cantidad: {producto.cantidad}</p>
+        </article>
+      ))}
+    </div>
+  );
+};
+
+export default Productos;
